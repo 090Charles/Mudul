6d18918a7ca122131824314e5ba3756875cda5f7
JeffryRomero <jeffryromero@unitec.edu>
2015-03-12 11:45:24 -0600

    Controlador y vista
    agrege el controlador y vista index de revisar calificaciones, todavia
    falta programar las vistas

--- MudulProject/MudulProject/Controllers/RevisarCalificacionesController.cs
+++ MudulProject/MudulProject/Controllers/RevisarCalificacionesController.cs
@@ -1,39 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
 
 namespace MudulProject.Controllers
 {
-    public class RevisarCalificacionesController : ApiController
+    public class RevisarCalificacionesController : Controller
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        //
+        // GET: /RevisarCalificaciones/
+        public ActionResult Index()
         {
-            return new string[] { "value1", "value2" };
+            return View();
         }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
-    }
+	}
 }
