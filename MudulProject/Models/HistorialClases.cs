ddbca4292373af006183000bde1e74533901d376
090Charles <charmarch_93@hotmail.com>
2015-03-16 13:57:52 -0600

    Creación de Historial de Prueba
    Se muestra una table de muestra

--- MudulProject/Models/HistorialClases.cs
+++ MudulProject/Models/HistorialClases.cs
@@ -12,18 +12,6 @@ namespace MudulProject.Models
     public class HistorialClases
     {
         public DataTable tabla { get; set; }
-        public DataTable Retornar_Tabla(int IdAlum)
-        {
-            
-            var sqlQuery = new SQLQuery();
-            string queryString = @" SELECT L.id,L.Description,L.Id_Carrera
-                                    FROM Matriculas M JOIN AsignaturasMatriculadas A 
-                                    ON M.Id=A.Id_Matricula JOIN Asignaturas L 
-                                    ON A.Id_Asignaturas=L.Id
-                                    WHERE M.NumberAccountId_Usuarios=3";
-
-            DataTable lista = sqlQuery.getTable(queryString);
-            return lista;
-        }
+     
     }
 }
