563bdf1a3f1f7ffd7198f2a8e889850b9967861d
JeffryRomero <jeffryromero@unitec.edu>
2015-03-18 23:03:01 -0600

    Vista Calificaciones Alumno

--- MudulProject/Controllers/RevisarCalificacionesController.cs
+++ MudulProject/Controllers/RevisarCalificacionesController.cs
@@ -10,14 +10,14 @@ namespace MudulProject.Controllers
 {
     public class RevisarCalificacionesController : Controller
     {
-        int idtipo;
-        int idusuario;
+        int idtipo=1;
+        int idusuario=3;
         //
         // GET: /RevisarCalificaciones/
         public ActionResult Index()
         {
             var query = new SQLQuery();
-            string qstring = @"Select ac.Description as Asignatura, u.Nombre, axa.HoraSubida as [Hora Subida], axa.HoraCalificacion as [Hora Calificacion], axa.Nota
+            string qstring = @"Select axa.Id, ac.Description as Actividad, u.Nombre, axa.HoraSubida as [Hora Subida], axa.HoraCalificacion as [Hora Calificacion], axa.Nota
                             from ActividadXAlumno axa 
                             join Actividades ac on axa.Id_actividad=ac.Id
                             join Usuarios u on axa.Id_alumno=u.NumberAccountId";
@@ -25,7 +25,30 @@ namespace MudulProject.Controllers
             ViewBag.Tabla = lista;
             return View();
         }
+
+        public ActionResult IndexAlumno()
+        {
+            if (idtipo == 1)
+            {
+                var query = new SQLQuery();
+                string qstring = @"Select axa.Id, ac.Description as Actividad, u.Nombre, axa.HoraSubida as [Hora Subida], axa.Nota
+                            from ActividadXAlumno axa 
+                            join Actividades ac on axa.Id_actividad=ac.Id
+                            join Usuarios u on axa.Id_alumno=u.NumberAccountId
+                            where axa.Id_alumno=" + idusuario.ToString();
+                DataTable lista = query.getTable(qstring);
+                ViewBag.Tabla = lista;
+                return View();
+            }
+            else
+                return Redirect("Index");
+        }
+
         public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit(int id)
         {
             return View();
         }
