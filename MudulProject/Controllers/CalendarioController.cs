645b19aa2b252d9908c965ee076db8e383712856
Jeries Handal <JeriesHG@Gmail.com>
2015-03-24 23:31:46 -0600

    Volver a comitear calendario en la raiz

---
+++ MudulProject/Controllers/CalendarioController.cs
@@ -0,0 +1,33 @@
+﻿using MudulProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+
+namespace MudulProject.Controllers
+{
+    public class CalendarioController : Controller
+    {
+        private MoodleConnection db = new MoodleConnection();
+
+        // GET: Calendario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [OutputCache(Duration=3600, VaryByParam="none", Location=OutputCacheLocation.Client, NoStore=true)]
+        public ActionResult AjaxTest()
+        {
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(db.Actividades.ToList());
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MudulProject/Scripts/calendarioJS.js
@@ -0,0 +1,68 @@
+﻿$(document).ready(function () {
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        today: true,
+        displayEventEnd: true,
+        eventLimit: true, // allow "more" link when too many events
+        cache: true,
+        lazyFetching: true,
+        events: function (start, end, timezone, callback) {
+            $.ajax({
+                url: '/Calendario/AjaxTest',
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                cache:true,
+                success: function (data) {
+                    var doc = JSON.parse(data);
+                    var events = [];
+                    $(doc).each(function () {
+                        var horaInicio = parseDateToJS($(this).attr('HoraInicio'));
+                        var horaLimite = parseDateToJS($(this).attr('HoraLimite'));
+                        if (!isWorkingHour(horaLimite)) {
+                            horaLimite.setDate(horaLimite.getDate() + 1);
+                        }
+
+                        events.push({
+                            id:$(this).attr('Id'),
+                            title: $(this).attr('Titulo'),
+                            start: horaInicio,
+                            end: horaLimite,
+                            color: '#009688',
+                            eventBackgroundColor: '#009688',
+                            eventTextColor: '#009688'
+                        });
+                    });
+                    callback(events);
+                }
+            });
+        },
+        eventClick: function (calEvent, jsEvent, view) {
+            /*window.location = 'page.php?'
+                              + 'id=' + calEvent.id
+                              + '&start=' + calEvent.start
+                              + '&end=' + calEvent.end
+                              + '&user_id=' + calEvent.user_id;*/
+            alert('Redirect con estos datos: id=' + calEvent.id
+                              + '&start=' + calEvent.start
+                              + '&end=' + calEvent.end);
+        }
+    });
+
+    function parseDateToJS(toParseDate) {
+        var jsonDate = toParseDate;
+        var re = /-?\d+/;
+        var m = re.exec(jsonDate);
+        var formattedDate = new Date(parseInt(m[0]));
+        return formattedDate;
+    }
+
+    function isWorkingHour(now) {
+        return now.getHours() >= 9 && now.getHours() < 24;
+    }
+});
